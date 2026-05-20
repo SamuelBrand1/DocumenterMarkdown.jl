7e1afb826a24b23a0c11748fa942200832bf3b97
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2026-05-20 12:20:40 +0100

    Switch writer to MarkdownAST and update API

--- src/DocumenterMarkdown.jl
+++ src/DocumenterMarkdown.jl
@@ -1,14 +1,11 @@
 module DocumenterMarkdown
-using Documenter: Documenter
-using Documenter.Utilities: Selectors
-
-const ASSETS = normpath(joinpath(@__DIR__, "..", "assets"))
+using Documenter: Documenter, Selectors
 
 include("writer.jl")
 export Markdown
 
-# Selectors interface in Documenter.Writers, for dispatching on different writers
-abstract type MarkdownFormat <: Documenter.Writers.FormatSelector end
+# Selectors interface — dispatches `format = Markdown()` on `makedocs` to our render function.
+abstract type MarkdownFormat <: Documenter.FormatSelector end
 Selectors.order(::Type{MarkdownFormat}) = 1.0
 Selectors.matcher(::Type{MarkdownFormat}, fmt, _) = isa(fmt, Markdown)
 Selectors.runner(::Type{MarkdownFormat}, fmt, doc) = render(doc, fmt)
